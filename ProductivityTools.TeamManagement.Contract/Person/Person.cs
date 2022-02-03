04fd81ca3ac0d03f70949813f727298659dd519a
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-02-03 22:48:02 +0100

    contract

---
+++ ProductivityTools.TeamManagement.Contract/Person/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.TeamManagement.Contract
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Initials { get; set; }
+        public string Category { get; set; }
+    }
+}
